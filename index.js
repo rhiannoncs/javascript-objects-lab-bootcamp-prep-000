3db258575e5271468d2de5db1be8bdaa7c59a6ae
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-07 23:44:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var recipes = { }
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
